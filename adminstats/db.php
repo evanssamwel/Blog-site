7993bc3093fb38e28e2047fc5eeef2cfa20e0d75
EvansSamuel <snyamuevans@gmail.com>
2025-06-19 12:49:03 +0300

    updates

--- adminstats/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-$conn = mysqli_connect("localhost", "resblog_user", "StrongPassword123!", "blog_admin_db");
-// Check connection
-if (mysqli_connect_errno())
-  {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  }
- ?>
--- database/db_connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Example MySQLi connection
-$con = mysqli_connect("localhost", "resblog_user", "StrongPassword123!", "blog_admin_db");
-
-if (!$con) {
-    die("MySQLi connection failed: " . mysqli_connect_error());
-}
-?>
