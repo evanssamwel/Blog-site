b01b161594f79b2a1267108c2de2285afebefb2b
EvansSamuel <snyamuevans@gmail.com>
2025-06-19 12:37:45 +0300

    Added or updated: adminstats/connect.php

---
+++ adminstats/connect.php
@@ -0,0 +1,5 @@
+<?php
+$dsn = 'mysql:host=localhost;dbname=blog_admin_db'; // DB host and name
+$username = 'resblog_user';  // new MySQL user
+$password = 'StrongPassword123!';  // password for new user
+?>
